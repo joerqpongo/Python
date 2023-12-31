5c9d3ae6851148720a1a0d03e1b2f3926ca2fe2d
Jose Romero <joerqpongo@gmail.com>
2023-12-31 09:53:34 +0100

    Actualizar

---
+++ PdfBuscarTexto.py
@@ -0,0 +1,27 @@
+# Importar la biblioteca PyPDF2
+import PyPDF2
+
+# Abrir el archivo PDF en modo binario de lectura
+file = open("documento.pdf", "rb")
+
+# Crear un objeto PdfFileReader
+pdfreader = PyPDF2.PdfFileReader(file)
+
+# Recorrer todas las páginas del PDF
+for i in range(pdfreader.numPages):
+
+    # Obtener la página actual
+    page = pdfreader.getPage(i)
+
+    # Extraer el texto de la página
+    text = page.extractText()
+
+    # Buscar el texto deseado
+    if "45520 OK" in text:
+
+        # Imprimir el número de página donde se encontró el texto
+        print(f"El texto '45520 OK' se encontró en la página {i + 1}")
+
+# Cerrar el archivo PDF
+file.close()
+
